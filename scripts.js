4b4035eb503935b6ebae6ff7a501c91f37085857
teohun0 <teohun0@gmail.com>
2026-02-06 21:47:46 +0800

    Enhance intersection observer to manage active dots
    Added functionality to highlight active dots based on section visibility.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll(".year");
+  const dots = document.querySelectorAll(".dot");
 
   const observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
+
+          const index = entry.target.getAttribute("data-index");
+          dots.forEach(dot => dot.classList.remove("active"));
+          if (dots[index]) dots[index].classList.add("active");
         }
       });
     },
-    {
-      threshold: 0.35
-    }
+    { threshold: 0.5 }
   );
 
-  sections.forEach(section => {
-    observer.observe(section);
-  });
+  sections.forEach(section => observer.observe(section));
 });
-
